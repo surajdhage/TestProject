b72501ebd43b57286d47e30627152bf8898910fa
surajdhage <87354011+surajdhage@users.noreply.github.com>
2021-07-14 17:22:23 +0530

    q8

---
+++ TwoNumber.java
@@ -0,0 +1,31 @@
+package Test;
+
+	import java.util.Scanner;
+	import java.lang.Math;
+	public class TwoNumber
+	{
+		public static void main (String[] args) 
+		{
+	        Scanner sc=new Scanner(System.in);
+	        int t=sc.nextInt();
+	         int a;
+	         int b;
+	         int n;
+	         int min;
+	         int max;
+	        for(int i=0;i<t;i++){
+	            a=sc.nextInt();
+	            b=sc.nextInt();
+	            n=sc.nextInt();
+	                   if(n%2!=0){
+	                    a=a*2;   
+	 }
+	      max=Math.max(a,b);
+	      min=Math.min(a,b);
+	      int ans=max/min;
+	      System.out.println(ans);
+	        }
+	           
+
+	    }
+	}
